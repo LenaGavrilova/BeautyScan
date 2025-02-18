402c396f1c67767fa55e82f046bc9d2f3ea14f0e
LenaGavrilova <lena.gavrilova2408@mail.ru>
2025-02-18 22:07:21 +0400

    add start page

---
+++ beauty_scan_client/src/components/Home-page.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="home-container">
+    <div class="content-box">
+      <h1 class="title">Добро пожаловать!</h1>
+      <p class="subtitle">Зарегистрируйтесь или войдите, чтобы продолжить</p>
+      <router-link to="/register">
+        <button class="btn register-btn">Регистрация</button>
+      </router-link>
+      <router-link to="/login">
+        <button class="btn login-btn">Вход</button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+onMounted(() => {
+  const isAuthenticated = localStorage.getItem('auth_token');
+  if (isAuthenticated) {
+    router.push('/main');
+  }
+});
+</script>
+
+<style scoped>
+.home-container {
+  min-height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Arial', sans-serif;
+  background: linear-gradient(135deg, #c6e3ff, #eee1ff);
+}
+
+.content-box {
+  background: rgb(255, 255, 255);
+  backdrop-filter: blur(10px);
+  border-radius: 20px;
+  padding: 40px;
+  text-align: center;
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+
+}
+
+.title {
+  color: #000000;
+  font-size: 32px;
+  margin-bottom: 20px;
+}
+
+.subtitle {
+  color: #000000;
+  margin-bottom: 30px;
+}
+
+.btn {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 15px;
+  border: none;
+  border-radius: 25px;
+  font-size: 18px;
+  cursor: pointer;
+  transition: background 0.3s ease;
+}
+
+.register-btn {
+  background: #007bff;
+  color: white;
+}
+
+.register-btn:hover {
+  background: #03458b;
+}
+
+.login-btn {
+  background: #007bff;
+  color: white;
+}
+
+.login-btn:hover {
+  background: #03458b;
+}
+</style>
--- beauty_scan_client/src/router/index.js
+++ beauty_scan_client/src/router/index.js
@@ -4,19 +4,30 @@ import LoginForm from '../components/Login-form.vue';
 import NotFound from '../components/Not-found.vue';
 import MyAccount from '../components/My-account.vue';
 import MainPage from '../components/Main-page.vue';
+import HomePage from '../components/Home-page.vue';
 // Определение маршрутов
 const routes = [
     { path: '/register', component: RegisterForm },
     { path: '/login', component: LoginForm },
     { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
     { path: '/account', component: MyAccount },
-    { path: '/', component: MainPage },
+    { path: '/main', component: MainPage },
+    { path: '/', component: HomePage },
 ];
 
 // Создание маршрутизатора
 const router = createRouter({
     history: createWebHistory(),
     routes
+});
+
+router.beforeEach((to, from, next) => {
+    const isAuthenticated = localStorage.getItem('auth_token'); // Проверка авторизации
+    if (to.path === '/' && isAuthenticated) {
+        next('/main');
+    } else {
+        next();
+    }
 });
 
 export default router;
